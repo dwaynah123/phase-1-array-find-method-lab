447cd77d3ba4b1af0326e3b41cbce4439cede205
Dwayna Harris <dwaynaharris@gmail.com>
2023-05-06 06:24:05 -0400

    Practice using find method in functions

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+
+function superbowlWin(record) {
+    let wins;
+    if(wins = record.find(({result}) => result === "W")){
+      return wins.year;
+    } else{
+      return undefined;
+    }
+}
